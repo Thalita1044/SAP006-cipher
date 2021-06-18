51dca536cbbf7ce98426256db0ea982a00385ef0
Thalita Machado <thalitamh5@gmail.com>
2021-06-18 12:53:50 -0300

    cifrado-final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import cipher from './cipher.js';
-console.log(cipher);
+//console.log(cipher);
 
 
 
@@ -8,10 +8,11 @@ function cifra(evento) {
   evento.preventDefault(); // evita que pag atualiza//
   let offsetCifrar  = parseInt (document.getElementById("contador").value);
   let stringCifrar =  document.getElementById("mensagem_cifrar").value;
-  //  console.log(stringCifrar)
-   // console.log(offsetCifrar)
+  
+  
   const cryptograph = cipher.encode(offsetCifrar, stringCifrar);
- console.log(cryptograph);
+ document.getElementById("mensagem_cifrar").value = cryptograph;
+ //console.log(cryptograph);//
   
 }
 
@@ -19,12 +20,13 @@ function decifrar (evento)  {
   evento.preventDefault();
   let offsetDecifrar  =  parseInt (document.getElementById("contador").value);
   let stringDecifrar  = document.getElementById("mensagem_decifrar").value;
- // console.log(stringDecifrar)
-  //console.log(offsetDecifrar)
+ 
   const cryptograph = cipher.decode(offsetDecifrar, stringDecifrar);
-  console.log(cryptograph);
+   document.getElementById("mensagem_decifrar").value = cryptograph;
+  //console.log(cryptograph);
  
 }
+
 const msgCifrada = document.getElementById("enviar1");
 msgCifrada.addEventListener("click", cifra);
 
